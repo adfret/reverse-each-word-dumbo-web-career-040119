1409b5b39a97834cfee50621e7bc6eb8dc09850d
Ad Friedman <adien330@yahoo.com>
2019-03-04 20:01:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-len = []
-string.split.each do |i|
-len << i.reverse
-end
-len.join(" ")
+    len = []
+    string.split.each do |i|
+    len << i.reverse
+      end
+    len.join(" ")
 end
